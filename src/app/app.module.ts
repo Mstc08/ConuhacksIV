51a7b5a8ce23c86d73d3593343eb8b5d30da60ef
May Pham <doan.may.pham@gmail.com>
2019-01-26 18:08:48 -0500

    created new components for home and nav-bar hide the overflow (scrollable)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/mat
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
