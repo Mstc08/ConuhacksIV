63fd9d3bff2ba4adeb467722113a158600c43060
May Pham <doan.may.pham@gmail.com>
2019-01-27 11:19:03 -0500

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import {MatButtonModule, MatToolbarModule} from '@angular/material';
-
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -27,7 +29,10 @@ import { ResultComponent } from './result/result.component';
     BrowserAnimationsModule,
     FileDropModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatAutocompleteModule,
+    HttpClientModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, throwError } from "rxjs";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { map, catchError } from 'rxjs/operators';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { UrlSerializer } from '@angular/router';
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
   styleUrls: ['./information.component.scss']
 })
 export class InformationComponent implements OnInit {
-
-  constructor() { }
+  options: any;
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+  };
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    
+    this.getListOfGroup("https://data.edmonton.ca/resource/kx42-g5ky.json").subscribe(data => {
+      this.options = data;
+      console.log(this.options);
+     });
   }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code. The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError(error);
+  }
+  public getListOfGroup(url: string): Observable<any> {
+    return this.http.get(url, this.httpOptions).pipe(
+      map((data)=>{
+        this.options = data;
+      },
+      catchError(this.handleError)
+    ));
+    // return this.http.get(url)
+                 
+  }
 }
